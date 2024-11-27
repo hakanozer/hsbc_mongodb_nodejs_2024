0be78b16f5545ef6faa2a2d5c2411ff01e530a6c
Hakan ÖZER <hakanozer02@gmail.com>
2024-11-27 12:16:27 +0300

    Update userService.ts

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -37,8 +37,8 @@ export const saveUserAll = async ( userArr: IUser[] ) => {
 export const updateUser = async (user: IUser) => {
     const updateUser = await User.findOneAndUpdate(
         { _id: user._id },
-        { $set: { uid: user.uid, name: user.name } },
-        //{ $set: user },
+        // { $set: { uid: user.uid, name: user.name, email: user.email } },
+        { $set: user },
         { new: true }
     )
     return updateUser
