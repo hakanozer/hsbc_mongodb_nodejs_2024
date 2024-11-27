b994093aa6209d0156452a21d5439991297b1b52
Hakan ÖZER <hakanozer02@gmail.com>
2024-11-27 11:51:03 +0300

    Update

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express"
-import { saveAllUser, saveUser, userLogin } from "../controllers/userController"
+import { saveAllUser, saveUser, userLogin, userUpdate } from "../controllers/userController"
 
 const userRouter = express.Router()
 
 userRouter.post('/create', saveUser)
 userRouter.post('/login', userLogin)
 userRouter.post('/createall', saveAllUser)
-userRouter.put('/update', saveAllUser)
+userRouter.put('/update', userUpdate)
 
 export default userRouter
